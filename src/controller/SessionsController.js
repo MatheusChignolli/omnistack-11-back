eb08ff9fe03ac5d6dca0b59f3638d04b9b084799
MatheusChignolli <matheuschignolli@gmail.com>
2020-03-27 14:16:18 -0300

    Omnistack - Week 11 - Backend

---
+++ src/controller/SessionsController.js
@@ -0,0 +1,28 @@
+const conn = require("../database/connection");
+
+module.exports = {
+
+  async create(req, res) {
+
+    const { id } = req.body;
+
+    const ong = await conn("ongs")
+      .where("id", id)
+      .select("name")
+      .first();
+
+    if(!ong) {
+      return res.status(400).json({ error: "No ONG has been found with this ID." });
+    }
+
+    return res.json({ ong });
+
+  },
+
+  async delete(req, res) {
+
+    
+
+  }
+
+}
